1c3c69510f42ed908c9757565c2ab6ef1a12ddc5
kaur <kaur.ilison@gmail.com>
2021-11-27 16:40:38 +0200

    Esimene katsetus

---
+++ training/polls/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question, Training
+
+
+def index(request):
+    latest_trainings_list = Training.objects.order_by('-date')[:5]
+    template = loader.get_template('polls/trainings.html')
+    context = {
+        'latest_trainings_list': latest_trainings_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
